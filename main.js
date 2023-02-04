3d5d8733dd2ff23bfdcdd3b773eabb950e74c4d8
sid-bhargav <sid.bhargav@gmail.com>
2023-02-03 22:58:07 -0600

    Changed from react-native to electron

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } =require('electron')
+
+const url = require('url')
+const path = require('path')
+
+function createMainWindow () {
+  const mainWindow = new BrowserWindow({
+    title: 'Color Palette Creator',
+    width: 1000,
+    height: 600,
+  })
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+  })
+
+  mainWindow.loadURL(startUrl)
+}
+
+app.whenReady().then(createMainWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
